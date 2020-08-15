d376a661e44dd978c3ffcc4cf4ea1c97645a5c97
Nick Pisarski <nick.pisarski@gmail.com>
2020-08-15 17:24:17 -0500

    added inputs for the bfc

---
+++ bfc.js
@@ -0,0 +1,8 @@
+// Weight and nine measurements
+
+$('#bfc-calculate').click((evt) => {
+    evt.preventDefault();
+
+    const wt = parseFloat($('#bfcWeightInput').val());
+    console.log(wt);
+})
